25a872343db38cda7b6bba76d93dc13c07b408ad
nilanjandaw <nilanjandaw@gmail.com>
2020-04-04 21:03:31 +0530

    feat: added admin module
    bug: need to add authentication

--- app.js
+++ app.js
@@ -4,8 +4,50 @@ var cookieParser = require('cookie-parser');
 var fs = require('fs');
 var logger = require('morgan');
 const webpush = require('web-push');
-
 var indexRouter = require('./routes/index');
+const AdminBro = require('admin-bro')
+const AdminBroSequelize = require('admin-bro-sequelizejs')
+const AdminBroExpress = require('admin-bro-expressjs')
+const bcrypt = require('bcrypt')
+AdminBro.registerAdapter(AdminBroSequelize)
+const models = require('./models')
+let adminBro = new AdminBro({
+    databases: [models],
+    rootPath: '/admin',
+    branding: {
+        companyName: 'PPE Help'
+    },
+    resources: [{
+        resource: models.User,
+        options: {
+            properties: {
+                password: {
+                    type: 'string',
+                    isVisible: {
+                        list: false, edit: true, filter: false, show: false,
+                    }
+                },
+            },
+            actions: {
+                new: {
+                    before: async (request) => {
+                        console.log(request.payload);
+                        
+                        if (request.payload.password) {
+                            request.payload.record = {
+                                ...request.payload,
+                                password: await bcrypt.hash(request.payload.password, 10),
+                            }
+                        }
+                        return request
+                    },
+                }
+            }
+        }
+    }],
+})
+
+const router = AdminBroExpress.buildRouter(adminBro)
 
 const vapidKeys = {
     publicKey: fs.readFileSync("./server.pub").toString(),
@@ -25,7 +67,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-
+app.use(adminBro.options.rootPath, router)
 app.use('/', indexRouter);
 
 module.exports = app;
