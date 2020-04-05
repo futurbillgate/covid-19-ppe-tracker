d42ddf5a6c70bc31155bc2b0d944bb1031f6917e
Ananya Bahadur <ananya95@gmail.com>
2020-04-05 10:43:16 +0530

    refactor: Change language tone of form
    Removes hidden mode field (includes the radio buttons inside the form)

--- public/js/ppe-create.js
+++ public/js/ppe-create.js
@@ -4,25 +4,22 @@ $( "input[name=options]" ).on( "change", function() {
 	const mode = $( this ).attr("id");
   switch (mode) {
   case "availability":
-    $("#ppe_info_set span").text("We have an excess of");
+    $("#ppe_info_set span").text("You're willing to share");
     $("#ppe_info_set small").text("");
     $("#ppe_info_set i").removeClass().addClass("em em-gift_heart");
     $("#name_emoji").removeClass().addClass("em em-angel");
-    $("#request_mode").attr("value", "availability");
     break;
   case "requirement":
-    $("#ppe_info_set span").text("We're in need of");
+    $("#ppe_info_set span").text("You're in need of");
     $("#ppe_info_set small").text("");
-    $("#ppe_info_set i").removeClass().addClass("em em-truck");
+    $("#ppe_info_set i").removeClass().addClass("em em-package");
     $("#name_emoji").removeClass().addClass("em em-bust_in_silhouette");
-    $("#request_mode").attr("value", "requirement");
     break;
   case "manufacturing":
-    $("#ppe_info_set span").text("We build");
-    $("#ppe_info_set small").text("and supply");
+    $("#ppe_info_set span").text("You build and supply");
+    $("#ppe_info_set small").text("");
     $("#ppe_info_set i").removeClass().addClass("em em-factory");
     $("#name_emoji").removeClass().addClass("em em-female-factory-worker");
-    $("#request_mode").attr("value", "manufacturing");
     break;
   }
 });
